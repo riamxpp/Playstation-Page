b37adaf275c8519cb0581690e773ba93a73b3962
riamxpp <riamstefeson616@gmail.com>
2022-06-07 14:13:57 -0300

    Tornando o componente Inscreva responsivo

--- src/Components/Inscreva/InscrevaStyles.ts
+++ src/Components/Inscreva/InscrevaStyles.ts
@@ -9,9 +9,17 @@ const InscrevaSection = styled.section<InscrevaInterfaceSection>`
   align-items: center;
   justify-content: center;
   background-image: url(${(props) => props.backgroundImg});
-  background-size: cover;
+  background-size: 120%;
   background-position: 0 -130px;
   background-repeat: no-repeat;
+  @media (max-width: 900px) {
+    background-size: 200%;
+    height: 450px;
+    background-position: 50% 50%;
+  }
+  @media (max-width: 400px) {
+    background-size: 300%;
+  }
 `;
 
 const ContainerConteudo = styled.div`
@@ -26,6 +34,9 @@ const TituloInscreva = styled.h1`
   font-weight: 700;
   font-size: 2.25rem;
   text-align: center;
+  @media (max-width: 900px) {
+    width: 429px;
+  }
 `;
 
 const TextoInscreva = styled.p`
@@ -34,6 +45,9 @@ const TextoInscreva = styled.p`
   font-size: 1rem;
   word-wrap: break-word;
   line-height: 1.6;
+  @media (max-width: 900px) {
+    width: 366px;
+  }
 `;
 
 export { InscrevaSection, ContainerConteudo, TituloInscreva, TextoInscreva };
